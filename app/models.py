44c4faf11ed6e64b14fe79f016fbfee41e70eba8
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-12-03 13:09:48 -0600

    added log in componets and allows the user to upload thier desired image to their profile

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=100, default='Supreme Admin')
+
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
     
     def __str__(self):
         return self.user.username
+
+
+
